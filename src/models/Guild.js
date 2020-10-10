684076f9e4ae75ecd78bd3fe71c91ad3ceab168f
Sazzo <felipe@sazz.fail>
2020-10-10 13:15:03 -0300

    Remove rolling value in the Guild Schema.

--- src/models/Guild.js
+++ src/models/Guild.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const GuildSchema = new mongoose.Schema({
   _id: { type: String, required: true },
   name: { type: String, required: true },
-  rolling: { type: Boolean, required: true },
   bestUser: { type: String, required: false },
   premium: { type: Boolean, required: true },
 })
