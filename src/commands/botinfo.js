4ed8e88af5fc5394702e7d1571be8a543a969c76
Sazzo <felipe@sazz.fail>
2020-10-10 16:13:03 -0300

    Little refactor.

--- src/commands/botinfo.js
+++ src/commands/botinfo.js
@@ -15,14 +15,14 @@ module.exports = class BotInfo extends Command {
   }
 
   async run({ message }) {
-    const matchs = await Rooms.countDocuments({})
+    const matches = await Rooms.countDocuments({})
     const embed = new MessageEmbed()
       .setAuthor('Ritsu', this.client.user.displayAvatarURL())
       .setDescription(
-        `Hi! I am **Ritsu** and I am a bot based on the game **Anime Music Quiz** made with <:JavaScript:764559579153432616> **Javascript** using discord.js, your objective in the game is simple, I will play an opening or ending and you must guess what his anime is! I am on **${this.client.guilds.cache.size}** servers with **${matchs}** current matches right now!`
+        `Hi! I am **Ritsu** and I am a bot based on the game **Anime Music Quiz** made with <:JavaScript:764559579153432616> **Javascript** using discord.js, your objective in the game is simple, I will play an opening or ending and you must guess what his anime is! I am on **${this.client.guilds.cache.size}** servers with **${matches}** current matches right now!`
       )
       .setColor('#3677e0')
-      .setThumbnail("https://i.imgur.com/6a0amL5.jpg")
+      .setThumbnail('https://i.imgur.com/6a0amL5.jpg')
       .addFields(
         {
           name: '<:blobElegant:764561836174999572> Commands',
@@ -36,8 +36,9 @@ module.exports = class BotInfo extends Command {
         },
         {
           name: ':medal: Credits',
-          value: '**r/AnimeThemes** - For providing the themes for the bot to play.\n**Anime Music Quiz** - Of course, the biggest inspiration for this project.\n**kyuu-chan-hackweek** - Many parts of the code I was inspired by it.\n**Gabriel Bifano and Pedro Lyrio** - They helped me a lot in making decisions and ideas for the bot.',
-          inline: false
+          value:
+            '**r/AnimeThemes** - For providing the themes for the bot to play.\n**Anime Music Quiz** - Of course, the biggest inspiration for this project.\n**kyuu-chan-hackweek** - Many parts of the code I was inspired by it.\n**Gabriel Bifano and Pedro Lyrio** - They helped me a lot in making decisions and ideas for the bot.',
+          inline: false,
         }
       )
       .setFooter('sazz.fail | @FelipeSazz')
