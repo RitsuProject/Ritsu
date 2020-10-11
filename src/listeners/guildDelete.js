07ed22967b54e410003997ac8163aafa474cd4a0
Sazzo <felipe@sazz.fail>
2020-10-11 19:10:17 -0300

    Update Discord Bots stats on guildRemove.

--- src/listeners/guildDelete.js
+++ src/listeners/guildDelete.js
@@ -7,5 +7,8 @@ module.exports = class guildDelete {
   async run(guild) {
     const guild_ = await Guilds.findById(guild.id)
     guild_.remove()
+    if (process.env.VERSION === 'production') {
+      await botListPost(this.client.guilds.cache.size)
+    }
   }
 }
