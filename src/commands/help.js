8c9bde2a0eb4339f7967eec1c469907c5548ff06
Sazzo <felipe@sazz.fail>
2020-10-11 19:54:06 -0300

    Use the guild prefix in help command instaed of the default.

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js')
+const { Guilds } = require('../models/Guild')
 const { Command } = require('../structures/Command')
 
 module.exports = class Help extends Command {
@@ -13,23 +14,24 @@ module.exports = class Help extends Command {
   }
 
   async run({ message }) {
+    const guild = await Guilds.findById(message.guild.id)
     const embed = new MessageEmbed()
     embed.setTitle('Available commands')
     embed.setColor('#3486eb')
     embed.setDescription(
       '**How to Play**: When using the command to start the game, the bot will play a song from a random opening or ending (or if you specify the year of the themes in the start command) and whoever can guess the anime from the largest number of openings or endings according to the rounds , wins.'
     )
-    embed.addField('Commands', this.getCommands())
+    embed.addField('Commands', this.getCommands(guild.prefix))
 
     message.channel.send(embed)
   }
 
-  getCommands() {
+  getCommands(prefix) {
     return this.client.commands
       .filter((c) => !c.dev)
       .map(
         (c) =>
-          `\`${this.client.prefix}${c.name} ${
+          `\`${prefix}${c.name} ${
             c.fields === null
               ? ''
               : `${c.fields.map((f) => `<${f}>`).join(' ')}`
