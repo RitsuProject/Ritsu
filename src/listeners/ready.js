abe7c82a4b85ba9dc8bbffeaae842db356f01190
Sazzo <felipe@sazz.fail>
2020-10-11 23:38:11 -0300

    Remove all rooms on restart.

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,8 +1,12 @@
+const { Rooms } = require('../models/Room')
+const { Guilds } = require('../models/Guild')
 module.exports = class ready {
   constructor(client) {
     this.client = client
   }
   async run() {
+    await Guilds.updateMany({}, { rolling: false, currentChannel: null })
+    await Rooms.deleteMany({})
     this.client.user.setActivity(
       `${
         process.env.VERSION === 'canary'
