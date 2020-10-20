9d29e4f59775fa8c4eab451a0597718bb20b439c
Sazzo <felipe@sazz.fail>
2020-10-19 22:30:47 -0300

    Add some aliases.

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -6,7 +6,7 @@ module.exports = class Profile extends Command {
   constructor(client) {
     super(client, {
       name: 'profile',
-      aliases: [],
+      aliases: ["user"],
       description: 'Show your profile.',
       requiredPermissions: null,
       dev: false,
