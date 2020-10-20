e3c22af0d760fa659ed02ea3f5c659ce8c5a97b3
Sazzo <felipe@sazz.fail>
2020-10-20 17:20:50 -0300

    Abstract the answser embed.

--- src/services/GameService.js
+++ src/services/GameService.js
@@ -8,6 +8,7 @@ const { UserService } = require('./UserService')
 const stringSimilarity = require('string-similarity')
 const mal = require('mal-scraper')
 const phin = require('phin')
+const EmbedGen = require('../utils/EmbedGen')
 
 module.exports.GameService = class GameService {
   constructor(message, options = {}) {
@@ -157,23 +158,7 @@ module.exports.GameService = class GameService {
         this.bumpScore(id)
       })
 
-      const embed = new MessageEmbed()
-      if (animeData !== undefined) {
-        embed.setImage(animeData.picture)
-      } else {
-        embed.setDescription(
-          "I couldn't get the cover of this anime because of errors."
-        )
-      }
-      embed.setTitle(answser)
-      embed.setColor('#33e83c')
-      embed.setFooter(
-        `Type: ${randomTheme.type} ${
-          animeData.englishTitle != ''
-            ? `| English: ${animeData.englishTitle}`
-            : ''
-        }`
-      )
+      const embed = EmbedGen(answser, type, animeData)
 
       this.message.channel.send('The answser is...', { embed })
       this.message.channel.send(
