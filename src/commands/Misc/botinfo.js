8bcaf09a2f8a17931ab8bff5792fcec3f3b45b34
Sazzo <felipe@sazz.fail>
2020-11-15 12:56:48 -0300

    Added new botinfo locales and finish inserting locales in botinfo.

--- src/commands/Misc/botinfo.js
+++ src/commands/Misc/botinfo.js
@@ -25,45 +25,69 @@ module.exports = class BotInfo extends Command {
     const embed = new MessageEmbed()
       .setAuthor('Ritsu', this.client.user.displayAvatarURL())
       .setDescription(
-        `Hi! I am **Ritsu** and I am a bot based on the game **Anime Music Quiz** made with <:JavaScript:764559579153432616> **Javascript** using discord.js, your objective in the game is simple, I will play an opening or ending and you must guess what his anime is! I am on **${
-          this.client.guilds.cache.size
-        }** servers, i'm online since **${uptime.days()}d ${uptime.hours()}h ${uptime.minutes()}m ${uptime.seconds()}s** and with **${matches}** current matches right now!`
+        t('commands:botinfo.ritsuAbout', {
+          jsEmoji: '<:JavaScript:764559579153432616>',
+          servers: this.client.guilds.size,
+          days: uptime.days(),
+          hours: uptime.hours(),
+          minutes: uptime.minutes(),
+          seconds: uptime.seconds(),
+          matches: matches,
+        })
       )
       .setColor('#3677e0')
       .setThumbnail('https://files.catbox.moe/d6758e.png')
       .addFields(
         {
-          name: '<:blobElegant:764561836174999572> Commands',
-          value: 'Use the `help` command!',
+          name: `<:blobElegant:764561836174999572> ${t(
+            'commands:botinfo.commands'
+          )}`,
+          value: t('commands:botinfo.useHelpCommand'),
           inline: true,
         },
         {
-          name: '<:blobBlush:775007392772390922> Social',
+          name: `<:blobBlush:775007392772390922> Social`,
           value: `
           <:github:764559500941852692> [Github](https://github.com/RitsuProject/Ritsu)
           <:Twitter:764952510674763786> [Twitter](https://twitter.com/RitsuProject)
-          <:ritsuthink:764662176958906388> [Support Server](https://discord.gg/XuDysZg)
+          <:ritsuthink:764662176958906388> [${t(
+            'commands:botinfo.supportServer'
+          )}](https://discord.gg/XuDysZg)
           <:Patreon:764559661374242826> [Patreon](https://www.patreon.com/ritsubot)
           `,
           inline: true,
         },
         {
-          name: ':link: Useful links',
+          name: `:link: ${t('commands:botinfo.usefulLinks')}`,
           value: `
             <:discord:764952620842090537> [Invite](https://discord.com/oauth2/authorize/?permissions=3145728&scope=bot&client_id=763934732420382751)
-            <:discordbotlist:767376357600264192> [(Discord Bot List) Upvote the Bot!](https://discord.ly/ritsu-5101)
-            <:dbl:775006065250009129> [(top.gg) Upvote the Bot!](https://top.gg/bot/763934732420382751/vote)
+            <:discordbotlist:767376357600264192> [(Discord Bot List) ${t(
+              'commands:botinfo.upvote'
+            )}](https://discord.ly/ritsu-5101)
+            <:dbl:775006065250009129> [(top.gg) ${t(
+              'commands:botinfo.upvote'
+            )}}](https://top.gg/bot/763934732420382751/vote)
             `,
           inline: true,
         },
         {
-          name: ':medal: Credits',
+          name: `:medal: ${t('commands:botinfo.credits')}`,
           value: `
-            **r/AnimeThemes** - For providing the themes for the bot to play.
-            **openings.moe** - For providing the themes for the bot to play.
-            **Anime Music Quiz** - Of course, the biggest inspiration for this project.
-            **kyuu-chan-hackweek** - Many parts of the code I was inspired by it.
-            **Gabriel Bifano, FelipeSazz and Pedro Lyrio** - Ideas, development and a lot of patience, without these 3 people the bot probably wouldn't exist.
+            **r/AnimeThemes** - ${t(
+              'commands:botinfo.creditsDescriptions.themes'
+            )}
+            **openings.moe** - ${t(
+              'commands:botinfo.creditsDescriptions.themes'
+            )}
+            **Anime Music Quiz** - ${t(
+              'commands:botinfo.creditsDescriptions.animemusicquiz'
+            )}
+            **kyuu-chan-hackweek** - ${t(
+              'commands:botinfo.creditsDescriptions.kyuuchan'
+            )}
+            **Gabriel Bifano, FelipeSazz and Pedro Lyrio** - ${t(
+              'commands:botinfo.creditsDescriptions.creators'
+            )}
             `,
           inline: false,
         }
