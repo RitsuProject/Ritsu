32651b3c6852307f7ac141af70a0351a55812303
Sazzo <felipe@sazz.fail>
2020-12-05 16:20:24 -0300

    Fix leaderboard command.

--- src/commands/Social/leaderboard.js
+++ src/commands/Social/leaderboard.js
@@ -32,18 +32,28 @@ module.exports = class Leaderboard extends Command {
       .limit(10)
       .then((results) => {
         for (const result in results) {
-          console.log(`Fetching user ${results[result]._id}...`)
           const user = this.client.users.cache.get(results[result]._id)
           let fakeResult = parseInt(result)
           const rankNumber = fakeResult + 1
-          embed.addField(
-            `${rankNumber}.${user.tag}`,
-            `
+          if (user) {
+            embed.addField(
+              `${rankNumber}.${user.tag}`,
+              `
             Level: **${results[result].level}**
             Won Matches: **${results[result].wonMatches}**
             `,
-            true
-          )
+              true
+            )
+          } else {
+            embed.addField(
+              `${rankNumber}.Anonymous User`,
+              `
+            Level: **${results[result].level}**
+            Won Matches: **${results[result].wonMatches}**
+            `,
+              true
+            )
+          }
         }
       })
     message.channel.send(embed)
