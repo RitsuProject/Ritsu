3ff2d9a28d066410ba490ec6032048ccf97e9e77
Sazzo <felipe@sazz.fail>
2021-01-12 21:01:44 -0300

    Added a new Ritsu Title.

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { config } from 'dotenv'
+import { readFileSync } from 'fs'
 import RitsuClient from './src/structures/RitsuClient'
 
 config()
 const client = new RitsuClient(process.env.DISCORD_TOKEN)
 
 client.start().then(() => {
-  console.log('Client Started')
+  console.log(readFileSync('title.txt', 'utf8').toString())
+  console.log('---- Client Connected')
 })
