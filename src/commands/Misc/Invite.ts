0fdc353b4208c2ae51b8366ad88090a458da5a55
Sazzo <felipe@sazz.fail>
2021-02-12 18:20:58 -0300

    feat(commands): added invite command.

---
+++ src/commands/Misc/Invite.ts
@@ -0,0 +1,40 @@
+import { EmbedOptions, Message } from 'eris'
+import RitsuClient from 'src/structures/RitsuClient'
+import RitsuCommand from '../../structures/RitsuCommand'
+
+class Invite extends RitsuCommand {
+  constructor(client: RitsuClient) {
+    super(client, {
+      name: 'invite',
+      description: 'Add me to your server!',
+      dev: false,
+      aliases: [],
+      requiredPermissions: null,
+    })
+    this.client = client
+  }
+
+  async run(message: Message) {
+    const dmChannel = await message.author.getDMChannel()
+    let openDmChannel: boolean = true
+    dmChannel
+      .createMessage(
+        'Thanks for wanting to add me! Here is the invite link! <invite here owo>'
+      )
+      .catch(() => {
+        openDmChannel = false
+        message.channel.createMessage(
+          'It seems that your DM (Direct messages) is closed and that is why I was unable to send you the message.'
+        )
+      })
+      .then(() => {
+        if (openDmChannel) {
+          message.channel.createMessage(
+            'Could you have a look at your DM (Direct Messages) please?'
+          )
+        }
+      })
+  }
+}
+
+export = Invite
