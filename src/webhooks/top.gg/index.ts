f9eb9f2938f00ae27b516c2588febaa8a85b3fea
Sazzo <felipe@sazz.fail>
2021-03-28 10:22:57 -0300

    feat: added vote reward embed.

--- src/webhooks/top.gg/index.ts
+++ src/webhooks/top.gg/index.ts
@@ -26,7 +26,17 @@ export default async function handleDBL(
   }
   await user.save()
 
-  await dmChannel.createMessage('owo uwu owo uwu')
+  const embed = {
+    title: ':star: Thanks for voting!',
+    description:
+      'Thank you so much for giving me an upvote on top.gg! You have no idea how much it helps me.\n\n' +
+      "Because of your incredible help, I will give you a gift! You've earned +1 Cake and can use it to get your amazing hints when you're having trouble guessing the anime!\n\n" +
+      'Did you know that my patrons get more cakes when they upvote me? If you want, you can become one here! https://patreon.com/ritsubot\n\n' +
+      'I hope you have a lot of fun! Goodbye!',
+    color: 16755763, // Star Emoji Primary Color
+  }
+
+  await dmChannel.createMessage({ embed })
 
   return res.json({
     code: 200,
