451349c7736ddcb47677d1b8c42871615f29c05e
Sazzo <felipe@sazz.fail>
2021-04-08 14:52:48 -0300

    chore: change the Emoji.ts location.

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,4 +1,4 @@
-import Emojis from '@utils/GameUtils/Emojis'
+import Emojis from '@utils/Emojis'
 
 export default {
   DEFAULT_ERROR_MESSAGE: `${Emojis.AQUA_CRYING} | Oopsie! **A critical failure** happened and therefore **I was unable to continue/start the match!** If you think this should not be happening, contact us on the **Ritsu support server**! \n\`$e\``,
